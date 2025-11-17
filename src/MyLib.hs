a8c9c084e3788e4fed9b9708abdd3d84edcd31be
Will Badart <will@willbadart.com>
2025-11-17 14:57:26 -0800

    Add TcPlugin boilerplate

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,37 @@
-module MyLib (someFunc) where
+{-# LANGUAGE DataKinds #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module MyLib
+( plugin
+) where
+
+import qualified GHC.Plugins
+import GHC.TcPlugin.API
+  ( Ct
+  , TcPlugin(..)
+  , TcPluginM
+  , TcPluginRewriter
+  , TcPluginSolveResult
+  , TcPluginStage(..)
+  , TyCon
+  , UniqFM
+  , mkTcPlugin
+  )
+
+plugin :: GHC.Plugins.Plugin
+plugin = GHC.Plugins.defaultPlugin
+  { GHC.Plugins.tcPlugin = Just . mkTcPlugin . tcPlugin
+  }
+
+tcPlugin :: [String] -> GHC.TcPlugin.API.TcPlugin
+tcPlugin _args = TcPlugin
+  { tcPluginSolve = \() -> solve
+  , tcPluginRewrite = \() -> rewrite
+  , tcPluginInit = pure ()
+  , tcPluginStop = \_ -> pure ()
+  }
+
+solve :: [Ct] -> [Ct] -> TcPluginM 'Solve TcPluginSolveResult
+solve _givens _wanteds = undefined
+
+rewrite :: UniqFM TyCon TcPluginRewriter
+rewrite = undefined
