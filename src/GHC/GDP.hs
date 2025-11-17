2ec4e9bafb292841cc68a5747cae2c4c2f0524b8
Will Badart <will@willbadart.com>
2025-11-17 15:36:44 -0800

    Create red test that I wanna turn green
    Red in this case means "not compiling"

--- src/GHC/GDP.hs
+++ src/GHC/GDP.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 
-module GHC.GDP
-( plugin
-) where
+module GHC.GDP (plugin) where
 
 import qualified GHC.Plugins
 import GHC.Utils.Outputable (($$))
@@ -12,20 +10,25 @@ import GHC.TcPlugin.API
 plugin :: GHC.Plugins.Plugin
 plugin = GHC.Plugins.defaultPlugin
   { GHC.Plugins.tcPlugin = Just . mkTcPlugin . tcPlugin
+  , GHC.Plugins.pluginRecompile = GHC.Plugins.purePlugin
   }
+
+-- ==========
+
+type State = ()
 
 tcPlugin :: [String] -> GHC.TcPlugin.API.TcPlugin
 tcPlugin _args = TcPlugin
-  { tcPluginSolve = \() -> solve
-  , tcPluginRewrite = \() -> rewrite
+  { tcPluginSolve = solve
+  , tcPluginRewrite = rewrite
   , tcPluginInit = pure ()
   , tcPluginStop = \_ -> pure ()
   }
 
-solve :: [Ct] -> [Ct] -> TcPluginM 'Solve TcPluginSolveResult
-solve givens wanteds = do
-  tcPluginTrace "SOLVING" (ppr givens $$ ppr wanteds)
+solve :: State -> [Ct] -> [Ct] -> TcPluginM 'Solve TcPluginSolveResult
+solve () givens wanteds = do
+  tcPluginTrace "----- SOLVING -----" (ppr givens $$ ppr wanteds)
   pure $ TcPluginOk [] []
 
-rewrite :: UniqFM TyCon TcPluginRewriter
-rewrite = emptyUFM
+rewrite :: State -> UniqFM TyCon TcPluginRewriter
+rewrite () = emptyUFM
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,28 @@
 {-# OPTIONS_GHC -fplugin=GHC.GDP #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main (main) where
 
+import Logic.Proof
+import Logic.Propositional
+
+{-
+This tests whether the plugin's goal is met: to allow proof of a proposition to be used
+in place of a proof that it entails. In this case, we have a proof of `This && That`,
+which, naturally, entails `That`, which we try to provide to a function expecting proof
+of `That`.
+ -}
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = print $ needsThat proof
+
+-- ==========
+
+data This
+data That
+
+proof :: Proof (This && That)
+proof = axiom
+
+needsThat :: Proof That -> ()
+needsThat _ = ()
